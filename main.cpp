0022fcc032daafc188f32b83c376a3da7ee8ff1c
John Stewart Aslanides <john.aslanides@anu.edu.au>
2015-10-20 19:38:43 +1100

    Remove debug code from main

--- main.cpp
+++ main.cpp
@@ -43,11 +43,8 @@ void mainLoop(Agent &ai, Environment &env, options_t &options) {
 	// Agent/environment interaction loop
 
 	action_t action = 0;
-	//for (unsigned int cycle = 1; !env.isFinished(); cycle++) {
 	int cycle = 1;
 	while (true) {
-		//std::cout << "HistSize: " << ai.historySize() << " MaxTreeDepth: " << ai.maxTreeDepth() << std::endl;
-
 		// check for agent termination
 		if (terminate_check && ai.lifetime() > terminate_lifetime) {
 			aixi::log << "info: terminating lifetime" << std::endl;
@@ -57,8 +54,6 @@ void mainLoop(Agent &ai, Environment &env, options_t &options) {
 		// Get a percept from the environment
 		percept_t observation = env.getObservation();
 		percept_t reward = env.getReward();
-		//std::cout << "Reward = " << reward << std::endl;
-
 
 		// Update agent's environment model with the new percept
 
@@ -77,24 +72,12 @@ void mainLoop(Agent &ai, Environment &env, options_t &options) {
 		} else {
 			if (ai.historySize() >= ai.maxTreeDepth()) {
 				action = search(ai, 0.1);
-				//action = ai.genRandomAction();
-
 			} else {
 				action = ai.genRandomAction();
 				// std::cout << "Generating random action: " << action
 				//		<< std::endl;
 			}
 		}
-
-		// Send an action to the environment
-//		if (cycle == 1)
-//			action = 1; // move right 3, 3, 3, 2, 2, 2, 1, 1
-//		if (cycle == 2)
-//			action = 2; // move down  1, 2, 0, 0, 1, 3, 0, 3
-//		if (cycle == 3)
-//			action = 2; // move down
-//
-//		std::cout << "Performing action " << action << std::endl;
 		env.performAction(action);
 
 		// Update agent's environment model with the chosen action
@@ -119,14 +102,14 @@ void mainLoop(Agent &ai, Environment &env, options_t &options) {
 		if (explore)
 			explore_rate *= explore_decay;
 
-        cycle++;
+		cycle++;
 	}
 
 // Print summary to standard output
 
 	std::cout << std::endl << std::endl << "Episode finished. Summary:"
 			<< std::endl;
-	std::cout << "agent lifetime: " << ai.lifetime()+1 << std::endl;
+	std::cout << "agent lifetime: " << ai.lifetime() + 1 << std::endl;
 	std::cout << "average reward: " << ai.averageReward() << std::endl;
 
 }
@@ -209,9 +192,7 @@ int main(int argc, char *argv[]) {
 	for (int i = 0; i < n_episodes; i++) {
 		mainLoop(ai, *env, options);
 		env->envReset();
-		ai.contextTree()->debugTree();
 		ai.newEpisode();
-		ai.contextTree()->debugTree();
 	}
 
 	aixi::log.close();
