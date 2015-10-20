353bc14130ec0826802e2d1ef8cf611b2e787fb9
John Stewart Aslanides <john.aslanides@anu.edu.au>
2015-10-20 20:03:31 +1100

    Migrate experiment analysis to ipython notebook

--- final_report/experiment.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-data = pd.read_csv('../log.csv',sep=', ',engine='python')
-
-print data.head()
-wtf = np.array(data[['cycle','total reward']])
-print wtf
