c0fb32cc2da0f274d4df1419cfe5ce88bac1c0db
Jarryd Martin <u5712904@anu.edu.au>
2015-10-21 21:54:07 +1100

    bug fixes in tictactoe

--- environment.cpp
+++ environment.cpp
@@ -5,9 +5,10 @@
 #include <string>
 #include <bitset>
 #include <climits>
-
+#include "main.hpp"
 #include "util.hpp"
 
+using namespace aixi;
 
 class next;
 class next;
@@ -311,6 +312,12 @@ void TicTacToe::performAction(action_t action)
 	if (board[action] != 0 && freeCells != 0) //illegal move
 	{
 		m_reward = 0;
+		std::cout<< "illegal move ";
+		for(int r = 0; r<9; r++)
+			{
+				std::cout<< board[r] << " ";
+			}
+			std::cout << std::endl;
 		return; //Obverstaion will not change so there is no need to re-calculate
 	}
 	else
@@ -322,13 +329,25 @@ void TicTacToe::performAction(action_t action)
 			m_reward = 5;
 			m_observation = calBoardVal();
 			finished = 1;
+			std::cout<< "agent won ";
+			for(int r = 0; r<9; r++)
+			{
+				std::cout<< board[r] << " ";
+			}
+			std::cout << std::endl;
 			return;
 		}
-		else if (--freeCells == 0) //game is a draw
+		else if (freeCells == 0) //game is a draw
 		{
 			m_reward = 4;
 			m_observation = calBoardVal();
 			finished = 1;
+			std::cout << "game draw ";
+			for(int r = 0; r<9; r++)
+			{
+				std::cout<< board[r] << " ";
+			}
+			std::cout << std::endl;
 			return;
 		}
 		else
@@ -339,12 +358,37 @@ void TicTacToe::performAction(action_t action)
 				m_reward = 1;
 				m_observation = calBoardVal();
 				finished = 1;
+				std::cout << "agent lost ";
+				for(int r = 0; r<9; r++)
+				{
+					std::cout<< board[r] << " ";
+				}
+				std::cout << std::endl;
 				return;
 			}
-			else //game has not yet ended
+			else if (freeCells != 0) //game has not yet ended
 			{
 				m_reward = 3;
 				m_observation = calBoardVal();
+				std::cout << "game continues ";
+				for(int r = 0; r<9; r++)
+				{
+					std::cout<< board[r] << " ";
+				}
+				std::cout << std::endl;
+				return;
+			}
+			else
+			{
+				m_reward = 4;
+				m_observation = calBoardVal();
+				finished = 1;
+				std::cout << "game draw ";
+				for(int r = 0; r<9; r++)
+				{
+					std::cout<< board[r] << " ";
+				}
+				std::cout << std::endl;
 				return;
 			}
 		}
--- environment.hpp
+++ environment.hpp
@@ -175,36 +175,33 @@ private:
 
 	int check_winner()
 	{
-		if (freeCells <= 4)
+		if (board[0] != 0)
 		{
-			if (board[0] != 0)
-			{
-				if ((board[0] == board[1] && board[1] == board[2]) || (board[0] == board[3] && board[0] == board[6])
-					|| (board[0] == board[4] && board[4] == board[8]))
-					return board[0];
-			}
-			else if (board[1] != 0)
-			{
-				if (board[1] == board[4] && board[4] == board[7])
-					return board[1];
-			}
-			else if (board[2] != 0)
-			{
-				if ((board[2] == board[5] && board[5] == board[8]) || (board[2] == board[4] && board[4] == board[6]))
-					return board[2];
-			}
-			else if (board[3] != 0)
-			{
-				if (board[3] == board[4] && board[4] == board[5])
-					return board[3];
-			}
-			else if (board[6] != 0)
-			{
-				if (board[6] == board[7] && board[7] == board[8])
-					return board[6];
-			}
+			if ((board[0] == board[1] && board[1] == board[2]) || (board[0] == board[3] && board[0] == board[6])
+				|| (board[0] == board[4] && board[4] == board[8]))
+				return board[0];
 		}
-		return 0;
+		else if (board[1] != 0)
+		{
+			if (board[1] == board[4] && board[4] == board[7])
+				return board[1];
+		}
+		else if (board[2] != 0)
+		{
+			if ((board[2] == board[5] && board[5] == board[8]) || (board[2] == board[4] && board[4] == board[6]))
+				return board[2];
+		}
+		else if (board[3] != 0)
+		{
+			if (board[3] == board[4] && board[4] == board[5])
+				return board[3];
+		}
+		else if (board[6] != 0)
+		{
+			if (board[6] == board[7] && board[7] == board[8])
+				return board[6];
+		}
+	return 0;
 	}
 };
 
